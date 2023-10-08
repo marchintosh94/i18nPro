52acba349669f4e0f4457f8027b55d426613c04c
Marco Baratto <marcobaratto@icloud.com>
2023-10-08 11:36:16 +0200

    React test init, react example improvements
    - example: init main layout to display @marchintosh94/i18npro-react in
      action
    - test: init tests on @marchintosh94/i18npro-react

--- examples/i18nPro-react/src/components/Navbar.tsx
+++ examples/i18nPro-react/src/components/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
 
   return (
     <nav className="flex sm:justify-between justify-center flex-wrap h-28">
-        <img className="max-h-24 w-24 my-4 hidden sm:block" src="/mb_dark.svg" alt="i18nPro" />
-        <img className="max-h-24 w-56 sm:w-72  my-4" src="/i18nPro.png" alt="i18nPro" />
+        <img className="max-h-20 w-24 my-4 hidden sm:block" src="/mb_dark.svg" alt="i18nPro" />
+        <img className="max-h-20 w-56 sm:w-72  my-4" src="/i18nPro.png" alt="i18nPro" />
         <Speedometer size={'lg'} range={[0, totalMemory]} value={memory}>
           <div className="flex flex-col justify-center items-center">
             <h4 className="text-xs sm:text-base opacity-80 ">Memory Usage</h4>
--- react/src/hook/useI18nPro.ts
+++ react/src/hook/useI18nPro.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { i18nPro } from "@marchintosh94/i18n-pro";
-import { ChangeLanguage } from "@marchintosh94/i18n-pro/dist/types/i18npro.types";
+import { ChangeLanguage } from "@marchintosh94/i18n-pro";
 
 export const useI18nPro = () => {
   const [locale, setLocale] = useState<string>(i18nPro.defaultLocale);
@@ -16,9 +16,13 @@ export const useI18nPro = () => {
     return i18nPro.changeLanguage(...args).then(updateLocaleState)
   }
 
+  const t: typeof i18nPro.t = (value: string, ...args) => {
+    return i18nPro.t(value, ...args)
+  }
+
   return {
     locale,
-    t: i18nPro.t,
+    t,
     switchLanguage
   }
 }
