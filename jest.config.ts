7382352be977d856fba18b44122b13a9be412c0b
Marco Baratto <marcobaratto@icloud.com>
2024-03-03 14:31:01 +0100

    implemented code formatter and tests threshold

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,23 @@
-import {  type JestConfigWithTsJest } from 'ts-jest'
+import { type JestConfigWithTsJest } from 'ts-jest'
 import { defaults as tsjPreset } from 'ts-jest/presets'
 
 const config: JestConfigWithTsJest = {
   verbose: true,
   transform: {
-    ...tsjPreset.transform,
-  }
-};
+    ...tsjPreset.transform
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  testEnvironment: 'node'
+}
 
 export default config
